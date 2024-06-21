d8ff443b8fa54427a5051fb5d21903a8e97c7f01
Yngasarock <128844558+Yngasarock@users.noreply.github.com>
2024-06-21 10:33:19 -0600

    collaborator.py

--- collaborator.py
+++ collaborator.py
@@ -1,25 +1,14 @@
 # Libraries.
 import socket
 import os
-from Crypto.Cipher import AES # type: ignore
-import base64
 
 HOST = 'localhost'
 PORT = 8000
 DOWNLOAD_DIR = 'downloadsCollab'
-KEY = b'Sixteen byte key'  # 16 bytes key for AES-128
 
 # If path doesn't exists.
 if not os.path.exists(DOWNLOAD_DIR):
     os.makedirs(DOWNLOAD_DIR)
-
-def decrypt(data):
-    data = base64.b64decode(data)
-    nonce = data[:16]
-    tag = data[16:32]
-    ciphertext = data[32:]
-    cipher = AES.new(KEY, AES.MODE_GCM, nonce=nonce)
-    return cipher.decrypt_and_verify(ciphertext, tag)
 
 def list_recipes(sock):
     sock.send('LIST\n'.encode('utf-8'))
@@ -32,11 +21,10 @@ def download_recipe(sock):
     
     filesize = int(sock.recv(1024).decode('utf-8'))
     if filesize > 0:
-        data = sock.recv(filesize).decode('utf-8')
-        decrypted_data = decrypt(data)
         filepath = os.path.join(DOWNLOAD_DIR, filename)
         with open(filepath, 'wb') as f:
-            f.write(decrypted_data)
+            data = sock.recv(filesize)
+            f.write(data)
         print(f'Recipe {filename} downloaded successfully to {DOWNLOAD_DIR}.')
     else:
         print('File not found.')
